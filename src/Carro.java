b4c01da0879aeb7d92c72e52a60f9b2bf4109b17
Fernando Pinho <fernandoalbertopinho20@gmail.com>
2023-12-13 14:47:53 +0100

    reenviar

--- src/Carro.java
+++ src/Carro.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class Carro {
+abstract class Carro {
     private String marca;
     private String modelo;
 
@@ -16,6 +16,53 @@ class Carro {
 
     public String getModelo() {
         return modelo;
+    }
+
+    // Método abstrato para obter informações específicas do carro
+    public abstract String getInfoEspecifica();
+}
+
+class Honda extends Carro {
+    public Honda(String modelo) {
+        super("Honda", modelo);
+    }
+
+    @Override
+    public String getInfoEspecifica() {
+        return "Informações específicas para Honda";
+    }
+}
+
+class RangeRover extends Carro {
+    public RangeRover(String modelo) {
+        super("Range Rover", modelo);
+    }
+
+    @Override
+    public String getInfoEspecifica() {
+        return "Informações específicas para Range Rover";
+    }
+}
+
+class Lexus570 extends Carro {
+    public Lexus570(String modelo) {
+        super("Lexus", "570");
+    }
+
+    @Override
+    public String getInfoEspecifica() {
+        return "Informações específicas para Lexus 570";
+    }
+}
+
+class BMWX7 extends Carro {
+    public BMWX7(String modelo) {
+        super("BMW", "X7");
+    }
+
+    @Override
+    public String getInfoEspecifica() {
+        return "Informações específicas para BMW X7";
     }
 }
 
--- src/ListaDeCarros.java
+++ src/ListaDeCarros.java
@@ -3,24 +3,19 @@ import java.util.List;
 
 public class ListaDeCarros {
     public static void main(String[] args) {
-        // Exemplos de instâncias de carros
-        Carro hondaCivic = new Carro("Honda", "Civic");
-        Carro rangeRoverEvoque = new Carro("Range Rover", "Evoque");
-        Carro bmwSerie3 = new Carro("BMW", "Série 3");
-
         // Lista de carros
         List<Carro> listaDeCarros = new ArrayList<>();
-        listaDeCarros.add(hondaCivic);
-        listaDeCarros.add(rangeRoverEvoque);
-        listaDeCarros.add(bmwSerie3);
 
-        // Adicionar novos carros à lista
-        Carro novoCarro = new Carro("Toyota", "Corolla");
-        listaDeCarros.add(novoCarro);
+        // Adicionando carros à lista
+        listaDeCarros.add(new Honda("Civic"));
+        listaDeCarros.add(new RangeRover("Evoque"));
+        listaDeCarros.add(new Lexus570("LX"));
+        listaDeCarros.add(new BMWX7("2023"));
 
         // Imprimir informações sobre os carros na lista
         for (Carro carro : listaDeCarros) {
-            System.out.println("Marca: " + carro.getMarca() + ", Modelo: " + carro.getModelo());
+            System.out.println("Marca: " + carro.getMarca() + ", Modelo: " + carro.getModelo() +
+                    ", " + carro.getInfoEspecifica());
         }
     }
 }
